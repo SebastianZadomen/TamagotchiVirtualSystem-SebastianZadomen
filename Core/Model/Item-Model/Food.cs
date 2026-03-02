3011e4e6411a3cef79cc70772983921e448c212a
SebastianZadomen <sebastian.zadomen.7e8@itb.cat>
2026-03-02 12:27:01 +0100

    Enum created for food type, close #14

---
+++ Core/Model/Item-Model/Food.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TamagotchiVirtualSystem.Core.Model.Item_Model
+{
+    public class Food : Item
+    {
+         
+        public Food(string name, char icon, EState modifierState, int upgradeValue) : base(name, icon, modifierState, upgradeValue)
+        {
+        }
+    }
+}
